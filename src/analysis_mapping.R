bb451915e28669bcc1c56b76cb4d382090dee306
Lucas Alvarenga Costa <36195796+luucasac@users.noreply.github.com>
2018-09-01 16:44:09 -0300

    Add map functions in readme
    Added mapping functions from purrr package and apply family.
    Applied map and lapply to compare their performance with the as.character function, that returns a list.
    Also applied the same as.character function with map_chr and sapply that returns a char vector.

---
+++ src/analysis_mapping.R
@@ -0,0 +1,12 @@
+# Mircrobenchmark
+mb <- microbenchmark(
+	map = map(r$Operator, as.character),
+	lapply = lapply(r$Operator, as.character),
+	map_chr = map_chr(r$Operator, as.character), # the output here will be a char vector
+	sapply = sapply(r$Operator, as.character), # the output here will be a char vector
+	times = 1000L,
+	unit = "ms"
+)
+
+# Print markdown table
+paste("### mapping functions", "", to_markdown(mb), sep = "\n")
