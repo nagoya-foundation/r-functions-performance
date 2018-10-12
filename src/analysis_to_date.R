5453876ada9346885f392e78ab08fc4f6ea86541
Filipe Filardi <filipefilardi@gmail.com>
2018-10-12 14:09:48 -0300

    Fix date format

--- src/analysis_to_date.R
+++ src/analysis_to_date.R
@@ -1,8 +1,8 @@
 # Mircrobenchmark
 mb <- microbenchmark(
 	lubridate = dmy(r$Date),
-	to.date = as.Date(r$Date, '%d/%m%Y'),   # the output here will be a char vector
-	strptime = strptime(r$Date, '%d/%m%Y'),
+	to.date = as.Date(r$Date, '%d/%m/%Y'),   # the output here will be a char vector
+	strptime = strptime(r$Date, '%d/%m/%Y'),
 	times = 1000L,
 	unit = "ms"
 )
